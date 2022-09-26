620ee83ac3f916f116b1aecfceacc38924e46d55
summertree128 <natsukinoue530000@gmail.com>
2022-09-27 01:15:24 +0900

    [06]add layout tags

--- 06-memo-app-spa/src/MemoApp.js
+++ 06-memo-app-spa/src/MemoApp.js
@@ -4,6 +4,14 @@ function MemoApp() {
       <header className="memo-app-header">
         <p>Super Simple Memo App</p>
       </header>
+      <div className="memo-app-container">
+        <aside className="memo-app-sidebar">
+          This is side bar
+        </aside>
+        <div className="memo-app-content">
+          This is content
+        </div>
+      </div>
     </div>
   );
 }
