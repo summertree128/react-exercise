fc9054a2b16c5824f55108d3c64f7775382e912e
summertree128 <natsukinoue530000@gmail.com>
2022-09-27 00:49:17 +0900

    [06]Create MemoApp and remove unnecessary files

---
+++ 06-memo-app-spa/src/MemoApp.js
@@ -0,0 +1,11 @@
+function MemoApp() {
+  return (
+    <div className="memo-app">
+      <header className="memo-app-header">
+        <p>Super Simple Memo App</p>
+      </header>
+    </div>
+  );
+}
+
+export default MemoApp;
--- 06-memo-app-spa/src/index.js
+++ 06-memo-app-spa/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MemoApp from './MemoApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MemoApp />
   </React.StrictMode>
 );
 
