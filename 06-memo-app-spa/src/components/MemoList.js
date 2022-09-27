193477259f7621721c6bb3dac26e2342f79ebb7a
summertree128 <natsukinoue530000@gmail.com>
2022-09-27 11:11:30 +0900

    [06]add state in MemoList

--- 06-memo-app-spa/src/components/MemoList.js
+++ 06-memo-app-spa/src/components/MemoList.js
@@ -1,11 +1,24 @@
-function MemoList() {
-  return (
-    <ul>
-      <li>memo 1</li>
-      <li>memo 2</li>
-      <li>memo 3</li>
-    </ul>
-  );
+import React from "react";
+
+class MemoList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      memos: [{ id: 1, text: 'memo 1'}, { id: 2, text: 'memo 2'}]
+    }
+  }
+
+  render () {
+    return (
+      <ul>
+        {this.state.memos.map(memo => (
+          <li key={memo.id}>
+            {memo.text}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default MemoList;
