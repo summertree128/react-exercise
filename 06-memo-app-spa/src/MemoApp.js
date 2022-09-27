19816d322b8c8a35f5fb338467db769f12c1122c
summertree128 <natsukinoue530000@gmail.com>
2022-09-27 11:32:23 +0900

    [06]lift up state and add + button

--- 06-memo-app-spa/src/MemoApp.js
+++ 06-memo-app-spa/src/MemoApp.js
@@ -1,21 +1,39 @@
+import React from "react";
 import MemoList from "./components/MemoList";
 
-function MemoApp() {
-  return (
-    <div className="memo-app">
-      <header className="memo-app-header">
-        <p>Super Simple Memo App</p>
-      </header>
-      <div className="memo-app-container">
-        <aside className="memo-app-sidebar">
-          <MemoList />
-        </aside>
-        <div className="memo-app-content">
-          This is content
+ class MemoApp extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      memos: [{ id: 1, text: 'memo 1'}, { id: 2, text: 'memo 2'}],
+      editing: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  render () {
+    return (
+      <div className="memo-app">
+        <header className="memo-app-header">
+          <p>Super Simple Memo App</p>
+        </header>
+        <div className="memo-app-container">
+          <aside className="memo-app-sidebar">
+            <MemoList memos={this.state.memos}/>
+            <button onClick={this.handleClick}>+</button>
+          </aside>
+          <div className="memo-app-content">
+            This is content
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  handleClick (e) {
+    e.preventDefault()
+    this.setState({ editing: true })
+  }
 }
 
 export default MemoApp;
--- 06-memo-app-spa/src/components/MemoList.js
+++ 06-memo-app-spa/src/components/MemoList.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 class MemoList extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      memos: [{ id: 1, text: 'memo 1'}, { id: 2, text: 'memo 2'}]
-    }
+  constructor(props) {
+    super(props)
   }
 
   render () {
     return (
       <ul>
-        {this.state.memos.map(memo => (
+        {this.props.memos.map(memo => (
           <li key={memo.id}>
             {memo.text}
           </li>
