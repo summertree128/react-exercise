9a10482b5f86801e6217263dfafe9907d9d3eb1d
summertree128 <natsukinoue530000@gmail.com>
2022-10-06 00:24:32 +0900

    [07]delete contact

--- 07-react-router-tutorial/src/main.jsx
+++ 07-react-router-tutorial/src/main.jsx
@@ -17,6 +17,8 @@ import Root, {
 import EditContact, {
   action as editAction,
 } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+
 
 const router = createBrowserRouter([
   {
@@ -36,6 +38,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
