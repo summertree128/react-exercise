4f5fe9d4cdcadc6660191685935ee78aaa9c4aa4
summertree128 <natsukinoue530000@gmail.com>
2022-10-06 15:48:28 +0900

    [07]add the search spinner

--- 07-react-router-tutorial/src/routes/root.jsx
+++ 07-react-router-tutorial/src/routes/root.jsx
@@ -28,6 +28,12 @@ export default function Root() {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
   useEffect(() => {
     setQuery(q);
   }, [q]);
@@ -41,6 +47,7 @@ export default function Root() {
             <input
               type="search"
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               name="q"
@@ -50,7 +57,7 @@ export default function Root() {
                 submit(e.currentTarget.form)
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true}></div>
+            <div id="search-spinner" aria-hidden hidden={!searching}></div>
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
