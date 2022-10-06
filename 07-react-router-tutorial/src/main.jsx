e0e27f61e2ecd7351950acb678c0f7233a9f0e8f
summertree128 <natsukinoue530000@gmail.com>
2022-10-06 17:13:42 +0900

    [07]implement favorite button with fetcher form

--- 07-react-router-tutorial/src/main.jsx
+++ 07-react-router-tutorial/src/main.jsx
@@ -9,6 +9,7 @@ import './index.css'
 import ErrorPage from "./error-page";
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from "./routes/contact";
 import Root, {
   loader as rootLoader,
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
